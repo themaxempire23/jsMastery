8503714c0b9d2dd967448d439d948b2d2bf73da8
max <Phillemon.Haikali@transnamib.com.na>
2025-08-09 23:31:39 +0200

    learning variables in js

---
+++ jsBasics/variables.js
@@ -0,0 +1,30 @@
+// what are varibles? => Containers for storing data
+
+//first way of declaring variables
+fullName = "Tim Smithon";
+
+//2nd way of declaring variables
+var countryName = "Pakistan";
+
+//3rd way of declaring variables
+let cityName = "London";
+
+const appName = "My Project";
+//appName = "Demo Project";
+
+console.log(appName);
+
+// How to name variables
+// fullName
+// fullName
+// full_name
+// FullName
+// Full_Name
+
+
+
+// console.log(fullName)
+// console.log(countryName)
+// console.log(cityName)
+
+
