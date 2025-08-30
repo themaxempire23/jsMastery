32783d3a89f034269727dfefff2496d153e71963
max <Phillemon.Haikali@transnamib.com.na>
2025-08-30 12:43:03 +0200

    covered js variables

--- jsBasics/variables.js
+++ jsBasics/variables.js
@@ -6,13 +6,15 @@ fullName = "Tim Smithon";
 //2nd way of declaring variables
 var countryName = "Pakistan";
 
+countryName = 'Canada';
+
 //3rd way of declaring variables
 let cityName = "London";
 
-const appName = "My Project";
+//const appName = "My Project";
 //appName = "Demo Project";
 
-console.log(appName);
+//console.log(countryName);
 
 // How to name variables
 // fullName
@@ -28,3 +30,20 @@ console.log(appName);
 // console.log(cityName)
 
 
+var isActive = true //boolean datatype
+console.log(isActive);
+
+var isActive = false
+console.log(isActive);
+
+// Example of getting a mark:
+
+// var isActive = false
+var marks = 90;
+console.log(marks);
+
+var percentage = 85.6;
+console.log(percentage);
+
+var courseName = "JavaScript";
+console.log(courseName)
