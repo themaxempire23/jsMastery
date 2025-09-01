3abf4a6382ba3ab93325f5c89642b875fcf3a216
max <Phillemon.Haikali@transnamib.com.na>
2025-09-01 11:47:11 +0200

    we got this

---
+++ conditional Statements/if.js
@@ -0,0 +1,19 @@
+// sending an email to my friend
+
+ var temperature = 36;
+
+// Checking of temperature is = or less than < than 5
+if(temperature <= 5) {
+    console.log('Its cold go inside and wear a jacklot');
+}
+
+if(temperature <=10){
+    console.log("Its too cold now, please keep warm!")
+}
+
+if(temperature <= 35){
+    console.log("Its hot today, please keep cool!")
+}
+else{
+    console.log("Its hot today. Go inside!")
+}
--- jsBasics/operator_precedence.js
+++ jsBasics/operator_precedence.js
@@ -1,2 +1,4 @@
 console.log((5+7)*8);
 console.log((5+7)*8)+56/45-23;
+
+console.log(2 + "2")
