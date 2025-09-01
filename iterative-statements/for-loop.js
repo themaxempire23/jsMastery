ad167a6111c4e9b2d9f4be7563f882ed43f5a5d0
max <Phillemon.Haikali@transnamib.com.na>
2025-09-01 17:19:51 +0200

    for loops done

--- iterative-statements/for-loop.js
+++ iterative-statements/for-loop.js
@@ -7,6 +7,58 @@
 // }
 
 
-for(var i = 0; i <= 10; i++){
+for(var i = 1; i <= 10; i++){
+    if(i % 2 == 0){
     console.log(i);
+    }
+}
+
+
+
+
+// Example 2
+
+for(var i = 1; i <= 10; i++){
+    // if(i % 2 == 0){
+    // console.log(i);
+    // }
+    if (i == 5){
+        continue;
+    }
+    console.log(i)
+}
+
+
+// Example 3:
+
+// Example 2
+
+for(var i = 1; i <= 10; i++){
+    // if(i % 2 == 0){
+    // console.log(i);
+    // }
+    if (i == 5){
+        break;
+    }
+    console.log(i)
+}
+
+
+
+//Example 3 using loops with Arrays
+
+var teams = ['Chelsea', 'Man.U', 'Arsenal', 'City', 'Spurs', 'Villa'];
+
+// Example 2
+
+for(var i = 0; i <= teams.length; i++){
+    console.log(teams[i]);
+}
+
+
+//4th example:
+
+
+for(var i = 0; i < teams.length; i++){
+    console.log(teams[i]);
 }
