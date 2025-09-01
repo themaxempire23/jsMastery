148a198c4e27153d412d7a73a632998dedc33524
max <Phillemon.Haikali@transnamib.com.na>
2025-09-01 17:46:18 +0200

    ended here

---
+++ iterative-statements/while-loop.js
@@ -0,0 +1,15 @@
+var teams = ['Chelsea', 'Man.U', 'Arsenal', 'City', 'Spurs', 'Villa'];
+
+// for(var i = 0; i <= teams.length; i++){
+//     console.log(teams[i]);
+// }
+
+// while(condition){
+//     this is loop body
+// }
+
+var i = 0; 
+while(i < teams.length){
+    console.log(students[i])
+    i++;
+}
