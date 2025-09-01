31b4b581d1ea0e9fc9dc9010cb0248c7e8e30120
max <Phillemon.Haikali@transnamib.com.na>
2025-09-01 17:10:14 +0200

    iterative statements done

---
+++ iterative-statements/for-loop.js
@@ -0,0 +1,12 @@
+// How to code a for loop in js
+
+
+//---> for loo skeleto <---//
+// for(initialization; Condition; increment/decrement; ){
+//     this is a loop body
+// }
+
+
+for(var i = 0; i <= 10; i++){
+    console.log(i);
+}
