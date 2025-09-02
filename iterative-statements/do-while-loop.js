d62373eb319b793dfe454487d30cd76fef7f1fd4
max <Phillemon.Haikali@transnamib.com.na>
2025-09-02 10:20:30 +0200

    do while complete

---
+++ iterative-statements/do-while-loop.js
@@ -0,0 +1,21 @@
+var teams = ['Chelsea', 'Man.U', 'Arsenal', 'City', 'Spurs', 'Villa'];
+
+
+
+// Example 2: 
+
+
+// var i = 0; 
+// do{
+//     console.log(teams[i])
+//     i++;
+// }
+
+// while(i < teams.length);
+
+var i = 1
+do{
+    console.log(i);
+    i++
+}
+while(i<=0);
